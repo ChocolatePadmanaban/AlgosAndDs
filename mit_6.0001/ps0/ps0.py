a3772d27b0bc3cef30922f45f603a0b418839bbb
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2022-06-25 22:12:06 +0530

    from new laptop

---
+++ mit_6.0001/ps0/ps0.py
@@ -0,0 +1,10 @@
+import math
+
+def PS0():
+    x= float(input("Enter number x: "))
+    y= float(input("Enter number y: "))
+    print("""X**y =""", x**y)
+    print("log(x) =",math.log2(x))
+
+if __name__=="__main__":
+    PS0()
